bb24e4de15a4c70d46bee5d818be0dae8f299cef
Zareen Ara <asadzaraansari314@gmail.com>
2026-01-19 20:27:35 +0530

    Add files via upload

---
+++ Practical_Record/__init__.py
@@ -0,0 +1,4 @@
+# shapes/__init__.py
+
+from shapes.circle import area as circle_area, perimeter as circle_perimeter
+from shapes.rectangle import area as rectangle_area, perimeter as rectangle_perimeter
---
+++ Practical_Record/main.py
@@ -0,0 +1,13 @@
+from shapes import circle_area, circle_perimeter
+from shapes import rectangle_area, rectangle_perimeter
+
+# Circle
+r = 5
+print("Circle Area:", circle_area(r))
+print("Circle Perimeter:", circle_perimeter(r))
+
+# Rectangle
+l = 10
+w = 4
+print("\nRectangle Area:", rectangle_area(l, w))
+print("Rectangle Perimeter:", rectangle_perimeter(l, w))
